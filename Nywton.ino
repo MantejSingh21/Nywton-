3aeb666ad95a496003e761eb5d7a9c2037a2e7cc
MantejSingh21 <50661183+MantejSingh21@users.noreply.github.com>
2019-05-28 17:02:43 +0800

    Nywton
    Final working code with all changes made

---
+++ Nywton.ino
@@ -0,0 +1,211 @@
+
+#include <SparkFun_ADXL345.h>         // SparkFun ADXL345 Library
+
+/*********** COMMUNICATION SELECTION ***********/
+/*    Comment Out The One You Are Not Using    */
+//ADXL345 adxl = ADXL345(10);           // USE FOR SPI COMMUNICATION, ADXL345(CS_PIN);
+ADXL345 adxl = ADXL345();             // USE FOR I2C COMMUNICATION
+
+/****************** INTERRUPT ******************/
+/*      Uncomment If Attaching Interrupt       */
+//int interruptPin = 2;                 // Setup pin 2 to be the interrupt pin (for most Arduino Boards)
+
+
+/******************** SETUP ********************/
+/*          Configure ADXL345 Settings         */
+
+float temp;
+int sensorPin = 0;
+int piezoPin = 8;
+int inPin = 9;   // choose the input pin (for a pushbutton)
+int val = 0;
+int BP = 7;
+int GP = 5;
+int RP = 6;
+
+
+void setup(){
+  
+  Serial.begin(9600);                 // Start the serial terminal
+  Serial.println("Project Nywton");
+  Serial.println();
+  pinMode(8, OUTPUT);
+  pinMode(inPin, INPUT);
+  pinMode ( BP , OUTPUT );
+  pinMode ( GP , OUTPUT );
+  pinMode ( RP , OUTPUT );
+
+
+  adxl.powerOn();                     // Power on the ADXL345
+
+  adxl.setRangeSetting(16);           // Give the range settings
+                                      // Accepted values are 2g, 4g, 8g or 16g
+                                      // Higher Values = Wider Measurement Range
+                                      // Lower Values = Greater Sensitivity
+
+  adxl.setSpiBit(0);                  // Configure the device to be in 4 wire SPI mode when set to '0' or 3 wire SPI mode when set to 1
+                                      // Default: Set to 1
+                                      // SPI pins on the ATMega328: 11, 12 and 13 as reference in SPI Library 
+   
+  adxl.setActivityXYZ(1, 0, 0);       // Set to activate movement detection in the axes "adxl.setActivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
+  adxl.setActivityThreshold(75);      // 62.5mg per increment   // Set activity   // Inactivity thresholds (0-255)
+ 
+  adxl.setInactivityXYZ(1, 0, 0);     // Set to detect inactivity in all the axes "adxl.setInactivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
+  adxl.setInactivityThreshold(75);    // 62.5mg per increment   // Set inactivity // Inactivity thresholds (0-255)
+  adxl.setTimeInactivity(10);         // How many seconds of no activity is inactive?
+
+  adxl.setTapDetectionOnXYZ(0, 0, 1); // Detect taps in the directions turned ON "adxl.setTapDetectionOnX(X, Y, Z);" (1 == ON, 0 == OFF)
+ 
+  // Set values for what is considered a TAP and what is a DOUBLE TAP (0-255)
+  adxl.setTapThreshold(150);           // 62.5 mg per increment
+  adxl.setTapDuration(15);            // 625 μs per increment
+  adxl.setDoubleTapLatency(80);       // 1.25 ms per increment
+  adxl.setDoubleTapWindow(200);       // 1.25 ms per increment
+ 
+  // Set values for what is considered FREE FALL (0-255)
+  adxl.setFreeFallThreshold(5);       // (5 - 9) recommended - 62.5mg per increment
+  adxl.setFreeFallDuration(10);       // (20 - 70) recommended - 5ms per increment
+ 
+  // Setting all interupts to take place on INT1 pin
+  //adxl.setImportantInterruptMapping(1, 1, 1, 1, 1);     // Sets "adxl.setEveryInterruptMapping(single tap, double tap, free fall, activity, inactivity);" 
+                                                        // Accepts only 1 or 2 values for pins INT1 and INT2. This chooses the pin on the ADXL345 to use for Interrupts.
+                                                        // This library may have a problem using INT2 pin. Default to INT1 pin.
+  
+  // Turn on Interrupts for each mode (1 == ON, 0 == OFF)
+  adxl.InactivityINT(1);
+  adxl.ActivityINT(1);
+  adxl.FreeFallINT(1);
+  adxl.doubleTapINT(1);
+  adxl.singleTapINT(1);
+  
+//attachInterrupt(digitalPinToInterrupt(interruptPin), ADXL_ISR, RISING);   // Attach Interrupt
+
+}
+
+/****************** MAIN CODE ******************/
+/*     Accelerometer Readings and Interrupt    */
+void loop(){
+
+  val = digitalRead(inPin);  // read input value
+  if (val == HIGH) { 
+    Serial.println("*** Risk of Concussion ***");
+    Serial.println("**  Seek Medical Attention!***");
+    Serial.println();
+    tone(piezoPin, 100, 1500);
+  
+  }
+
+   //getting the voltage reading from the temperature sensor
+ int reading = analogRead(sensorPin);
+ 
+ 
+ // converting that reading to voltage, for 3.3v arduino use 3.3
+ float voltage = reading * 5.0;
+ voltage /= 1024.0; 
+ 
+ // print out the voltage
+ //Serial.print(voltage); Serial.println(" volts");
+ 
+ // now print out the temperature
+ float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                               //to degrees ((voltage - 500mV) times 100)
+ Serial.print(temperatureC); Serial.println(" degrees C");
+ 
+ // now convert to Fahrenheit
+// float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+// Serial.print(temperatureF); Serial.println(" degrees F");
+ 
+ delay(1000);                      
+  // Accelerometer Readings
+  int x,y,z;   
+  adxl.readAccel(&x, &y, &z);         // Read the accelerometer values and store them in variables declared above x,y,z
+
+  // Output Results to Serial
+  /* UNCOMMENT TO VIEW X Y Z ACCELEROMETER VALUES */  
+  //Serial.print(x);
+  //Serial.print(", ");
+  //Serial.print(y);
+  //Serial.print(", ");
+  //Serial.println(z); 
+  
+  ADXL_ISR();
+  // You may also choose to avoid using interrupts and simply run the functions within ADXL_ISR(); 
+  //  and place it within the loop instead.  
+  // This may come in handy when it doesn't matter when the action occurs. 
+
+}
+
+/********************* ISR *********************/
+/* Look for Interrupts and Triggered Action    */
+
+void ADXL_ISR() {
+  
+  // getInterruptSource clears all triggered actions after returning value
+  // Do not call again until you need to recheck for triggered actions
+  byte interrupts = adxl.getInterruptSource();
+  
+  // Free Fall Detection
+  if(adxl.triggered(interrupts, ADXL345_FREE_FALL)){
+    Serial.println("*** FREE FALL ***");
+    //add code here to do when free fall is sensed
+  } 
+  
+  // Inactivity
+  if(adxl.triggered(interrupts, ADXL345_INACTIVITY)){
+    Serial.println("*** INACTIVITY ***");
+     //add code here to do when inactivity is sensed
+  }
+  
+  // Activity
+  if(adxl.triggered(interrupts, ADXL345_ACTIVITY)){
+    Serial.println("*** ACTIVITY ***"); 
+     //add code here to do when activity is sense
+  }
+  
+  // Double Tap Detection
+  if(adxl.triggered(interrupts, ADXL345_DOUBLE_TAP)){
+    Serial.println("*** Possible Concussion ***");
+    tone(piezoPin, 250, 1500);
+       //digitalWrite (BP , HIGH );
+    //digitalWrite (GP , HIGH) ;
+    digitalWrite (RP , HIGH );
+    delay (500);
+    //digitalWrite (BP, LOW );
+    //digitalWrite (GP, LOW );
+    digitalWrite (RP , LOW );
+    tone(piezoPin, 250, 1500);
+  
+    
+  }
+  
+  // Tap Detection
+  if(adxl.triggered(interrupts, ADXL345_SINGLE_TAP)){
+    Serial.println("*** Risk of Concussion ***");
+    Serial.println("**  Seek Medical Attention!***");
+    Serial.println();
+    //digitalWrite (BP , HIGH );
+    //digitalWrite (GP , HIGH) ;
+    digitalWrite (RP , HIGH );
+    delay (500);
+    //digitalWrite (BP, LOW );
+    //digitalWrite (GP, LOW );
+    digitalWrite (RP , LOW );
+    tone(piezoPin, 250, 1500);
+ 
+  } 
+  else 
+  Serial.println();
+  Serial.print( "All good!" );
+  digitalWrite (BP , HIGH );
+  digitalWrite (GP , HIGH) ;
+  digitalWrite (RP , HIGH );
+  delay (50);
+  digitalWrite (BP, LOW );
+  digitalWrite (GP, LOW );
+  digitalWrite (RP , LOW );
+  
+   
+delay(50);
+}
+
+
